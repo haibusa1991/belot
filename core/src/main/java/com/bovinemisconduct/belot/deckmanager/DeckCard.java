22e44d8ee013316da9ef15fd3487c48af7be7287
Plamen Simeonov <haibusa1991@gmail.com>
2025-09-26 00:47:38 +0300

    added deck manager

---
+++ core/src/main/java/com/bovinemisconduct/belot/deckmanager/DeckCard.java
@@ -0,0 +1,38 @@
+package com.bovinemisconduct.belot.deckmanager;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class DeckCard {
+    private final Sprite cardSprite;
+    private final CardSuit cardSuit;
+    private final CardRank cardRank;
+
+    @Override
+    public String toString() {
+        String suit = switch (cardSuit) {
+            case CLUBS -> "♣";
+            case DIAMONDS -> "♦";
+            case HEARTS -> "♥";
+            case SPADES -> "♠";
+        };
+
+        String rank = switch (cardRank) {
+            case SEVEN -> "7";
+            case EIGHT -> "8";
+            case NINE -> "9";
+            case TEN -> "10";
+            case JACK -> "J";
+            case QUEEN -> "Q";
+            case KING -> "K";
+            case ACE -> "A";
+        };
+
+        return String.format("%s%s - %s", rank, suit, cardSprite);
+    }
+}
